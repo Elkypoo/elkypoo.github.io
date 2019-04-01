dfcc3a8088c178c568639978c591646398cdb7f5
Elkypoo <41938455+Elkypoo@users.noreply.github.com>
2019-03-31 22:35:22 -0700

    oh my fucking god

--- script.js
+++ script.js
@@ -56,9 +56,8 @@ function cookieCheck() {
 function tf2cwindow() {
     var iframeoverlay = document.getElementsByClassName("iframeoverlay");
     iframeoverlay[0].style.zIndex = "-2";
-    var tf2cpage = window.open("http://tf2center.com", "_blank", "height=100, width=100");
-    window.focus;
-    setTimeout(function () { tf2cpage.close(); writeCookie("tf2cloaded", "true", 4); location.reload(), 5750 });
+    var tf2cpage = window.open("http://tf2center.com", "TF2C Loading...", "height=100, width=100");
+    setTimeout(function () {tf2cpage.close(); writeCookie("tf2cloaded", "true", 4); location.reload()}, 5750 );
 };
 
 function tf2cCookieChecker() {
